655b0474872e5d6f20f4e6e19af1309345237846
Humphrey Hu <humphrey.hu@gmail.com>
2012-10-23 18:24:15 -0400

    Parameterized demo code.

--- demo.m
+++ demo.m
@@ -1,6 +1,9 @@
 % Demo script
-w1 = World2D([1,1]);
-w1.InitRobots(3);
+N = 100;
+world_size = 1;
+
+w1 = World2D([world_size,world_size]);
+w1.InitRobots(N);
 w1.PlotPoses
 cov = zeros(2);
 
@@ -11,12 +14,13 @@ w1.PlotMeasurements(ranges, angles);
 
 pause;
 
-w2 = World2D([1,1]);
-w2.InitRobots(3);
+w2 = World2D([world_size,world_size]);
+w2.InitRobots(N);
 cov = 1E-1*eye(3);
-noise = mvnrnd(zeros(3,3), cov);
-noise = reshape(noise, 3, 1, 3);
+noise = mvnrnd(zeros(N,3), cov);
+noise = reshape(noise, N, 1, 3);
 poses = w1.robot_poses + Pose2D(noise(:,:,1:2), noise(:,:,3));
+
 info = (1E-3*eye(2))^-1;
 for i = 1:10
    poses = GaussNewtonIterate(poses, ranges, angles, info);
