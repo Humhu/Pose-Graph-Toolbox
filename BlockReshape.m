7bbd5d77fd4fd79df7b358657c9d9cc60619ff1d
Humhu <humphrey.hu@gmail.com>
2013-01-21 20:37:48 -0500

    Added utility function to reshape block matrices.

---
+++ BlockReshape.m
@@ -0,0 +1,11 @@
+% Only intended to work for 2D matrices
+function [Mr] = BlockReshape(M, bsize, rsize)
+
+bheight = bsize(1);
+bwidth = bsize(2);
+num_blocks = size(M,1)*size(M,2)/(bheight*bwidth);
+
+pD = reshape(M, bheight, bwidth, num_blocks);
+pD = permute(pD, [2,1,3]);
+pD = reshape(pD, bwidth, *bheight);
+pD = permute(pD, [2,1]);
